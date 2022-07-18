f89c7633bb9600a5c7d49e9b43f426c4d1e98122
Matt Turner <mattturner1911@gmail.com>
2022-07-18 14:10:39 -0400

    Added math functions to the javascript file. Can use the operation in the console.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('test');
+// addition function
+let add = (num1, num2) => num1 + num2;
+
+// subtraction function
+let subtract = (num1, num2) => num1 - num2;
+
+// multiplication function
+let multiply = (num1, num2) => num1 * num2;
+
+// division function
+let divide = (num1, num2) => num1 / num2;
+
+// operator function
+let operation = (num1, num2, operator) => operator(num1, num2);
