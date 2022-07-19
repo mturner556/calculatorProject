78dcda8cd541a43c67df17a5e7792e6c19a710ff
Matt Turner <mattturner1911@gmail.com>
2022-07-19 08:06:18 -0400

    Modifiied the animation to fire on mousedown and mouseup events. Began to work on updating the textcontent of the display-text div with the user click of the number buttons.

--- app.js
+++ app.js
@@ -13,14 +13,25 @@ let divide = (num1, num2) => num1 / num2;
 // operator function
 let operation = (num1, num2, operator) => operator(num1, num2);
 
-// animation for button press 
+// document variables
 let buttonPress = document.getElementsByClassName('button');
+let numberButton = document.getElementsByClassName('number');
+let displayText = document.querySelector('#display-text');
 
+// animation for the individual buttons, and gets the textcontent of the element clicked
 for (let i = 0; i < buttonPress.length; i++) {
-    buttonPress[i].addEventListener('click', () => {
+    buttonPress[i].addEventListener('mousedown', () => {
         buttonPress[i].classList.add('button-press');
-        setTimeout(() => {
+        buttonPress[i].addEventListener('mouseup', () => {
             buttonPress[i].classList.remove('button-press');
-        }, 75);
+        });
+    });
+}
+
+// click event to update the display with the user input
+for (let i = 0; i < numberButton.length; i++) {
+    numberButton[i].addEventListener('click', () => {
+
+        
     });
 }
