fa0df7d4c5c19aa762d2f70dc0d51965b65dcac5
Matt Turner <mattturner1911@gmail.com>
2022-07-24 17:14:52 -0400

    Attached event listeners to the AC, DEL, and = buttons. The equal button evaulates the numbers put in by the user. AC clears the display and number variables. DEL deletes the last character of the currentNum and updates the display respectivly

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // addition function
-let add = (num1, num2) => num1 + num2;
+let add = (num1, num2) => Number(num1) + Number(num2);
 
 // subtraction function
-let subtract = (num1, num2) => num1 - num2;
+let subtract = (num1, num2) => Number(num1) - Number(num2);
 
 // multiplication function
-let multiply = (num1, num2) => num1 * num2;
+let multiply = (num1, num2) => Number(num1) * Number(num2)
 
 // division function
-let divide = (num1, num2) => num1 / num2;
+let divide = (num1, num2) => Number(num1) / Number(num2);
 
 // operator function
 let operate = (num1, num2, operator) => operator(num1, num2);
@@ -63,3 +63,40 @@ for (let i = 0; i < operatorButton.length; i++) {
         updatePreviouNumDisplay(e.target.textContent);
     });
 }
+
+// event listener to select the operator
+for (let i = 0; i < operatorButton.length; i++) {
+    operatorButton[i].addEventListener('click', (e) => {
+        operator = operatorButton[i].textContent;
+    });
+}
+
+// evaluate the operate button when the equal button is clicked
+equalButton.addEventListener('click', () => {
+    if (operator === '+') {
+        currentDisplayNumber.textContent = operate(previousNum, currentNum, add);
+    } else if (operator === '-') {
+        currentDisplayNumber.textContent = operate(previousNum, currentNum, subtract);
+    } else if (operator === 'x') {
+        currentDisplayNumber.textContent = operate(previousNum, currentNum, multiply);
+    } else if (operator === '÷') {
+        currentDisplayNumber.textContent = operate(previousNum, currentNum, divide);
+    } else {
+        console.log('error');
+    }
+});
+
+// clear all inputs when the AC button is clicked
+clearButton.addEventListener('click', () => {
+    currentNum = '';
+    previousNum = '';
+    currentDisplayNumber.textContent = '';
+    previousDisplayNumber.textContent = '';
+});
+
+// delete the last input when the DEL button is clicked
+deleteButton.addEventListener('click', () => { 
+    let newStr = currentNum.slice(0, -1);
+    currentNum = newStr;
+    currentDisplayNumber.textContent = newStr; 
+});
